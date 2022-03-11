96b7dc5544c4e33761079c7189c2e478e1f4ce87
Adrien <adrien.bony38@gmail.com>
2022-03-11 09:55:48 +0100

    insertion proche

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ville import Ville
 from utilitaires import *
-from cyberbrain import trace
+#from cyberbrain import trace
 from tkinter import *
 from affichage import *
 
@@ -40,53 +40,93 @@ def insertion_proche(listeV):
     if(listeVillesIP[0] in listeV and listeVillesIP[1] in listeV):
         listeV.remove(listeVillesIP[0])
         listeV.remove(listeVillesIP[1])
+        
+    for v in listeVillesIP:
+        print(v.getNum(), end=' ')
+        
+    print("Distance 1 : " + str(cout(listeVillesIP)))
+    
+    
+    distance_min = 9999999999
+    index_min = 999999999
+    ville = None
+    
+    for v in listeV:
+        for i in range(0, len(listeV)-1):
+            distance_ajoutee = calculDistance(listeV[i], v) + calculDistance(v, listeV[i+1]) - calculDistance(listeV[i], listeV[i-1])
+            print(distance_ajoutee)
+            print(distance_min)
+            if(distance_ajoutee < distance_min):
+                
+                distance_min = distance_ajoutee
+                index_min = i
+                ville = v
+                
+            
+    listeVillesIP.insert(index, ville)
+    for v in listeVillesIP:
+        print(v.getNum())
+    
+    
+    
+    
 
 
-    for i in range(0, len(listeV)):
+    # for i in range(0, 1): #len(listeV)
 
-        villeChoisie = listeV[0] #ville choisie au hasard
-        index = -1
+    #     villeChoisie = listeV[0] #ville choisie au hasard
+    #     index = -1
+    #     # distancePrecedente = 99999999.99
 
-        # on parcour toute la liste de ville pour trouver la prochaine ville 
-        # dont le cout sera le plus faible
-        for ville in listeV:
-            # on copie la liste de villes pour éviter les problèmes
-            # on calcule le cout de cette tournée
-            # puis on supprime la ville de la liste de test
-            distanceIndexOptimal = 99999999
-            distancePrecedente = 99999999
+    #     # on parcour toute la liste de ville pour trouver la prochaine ville 
+    #     # dont le cout sera le plus faible
+    #     for ville in listeV:
+            
+    #         # on copie la liste de villes pour éviter les problèmes
+    #         # on calcule le cout de cette tournée
+    #         # puis on supprime la ville de la liste de test
+    #         distanceIndexOptimal = 99999999
+    #         distancePrecedente = 99999999.99
+            
 
 
 
-            # on trouve l'index auquel la distance sera la plus petite
-            for pos in range(1, len(listeVillesIP)):
-                listeVillesIP.insert(pos, ville)
-                distanceTournee = cout(listeVillesIP)
-                listeVillesIP.remove(ville)
+    #         # on trouve l'index auquel la distance sera la plus petite
+    #         for pos in range(1, len(listeVillesIP)):
+    #             listeVillesIP.insert(pos, ville)
+    #             distanceTournee = cout(listeVillesIP)
+    #             listeVillesIP.remove(ville)
 
-                if(distanceTournee < distanceIndexOptimal):
-                    distanceIndexOptimal = distanceTournee
-                    index = pos
+    #             if(distanceTournee < distanceIndexOptimal):
+    #                 distanceIndexOptimal = distanceTournee
+    #                 index = pos
                 
 
-            # on insère la ville au meilleur index possible
-            listeVillesIP.insert(index, ville)
-            # puis on recalcule le cout de la tournée
-            distanceTournee = cout(listeVillesIP)
-            # print(f'Distance : {distanceTournee}, ville : {ville}')
-            listeVillesIP.remove(ville)
+    #         # on insère la ville au meilleur index possible
+    #         listeVillesIP.insert(index, ville)
+    #         # puis on recalcule le cout de la tournée
+    #         distanceTournee = cout(listeVillesIP)
+    #         # print(f'Distance tournee {distanceTournee}, vile : {ville.getNum()}')
+    #         # print(f'Distance : {distanceTournee}, ville : {ville}')
+    #         listeVillesIP.remove(ville)
 
 
-            # si le cout de cette tournée est inférieur au cout de la tournée précédente
-            # on considère que cette tournée est (pour le moment) la meilleure
-            if(distanceTournee < distancePrecedente):
-                if(villeChoisie in listeVillesIP):
-                    listeVillesIP.remove(villeChoisie)
-                villeChoisie = ville
-                distancePrecedente = distanceTournee
-                listeVillesIP.insert(index, villeChoisie)
+    #         # si le cout de cette tournée est inférieur au cout de la tournée précédente
+    #         # on considère que cette tournée est (pour le moment) la meilleure
+    #         if(distanceTournee < distancePrecedente):
+    #             print(f"inférieur {distanceTournee}, precedente = {distancePrecedente}")
+    #             distancePrecedente = distanceTournee
+                
+    #             if(villeChoisie in listeVillesIP):
+    #                 listeVillesIP.remove(villeChoisie)
+    #             villeChoisie = ville
+    #             listeVillesIP.insert(index, villeChoisie)
+    #     for v in listeVillesIP:
+    #         print(v.getNum(), end=' ')
+    #     # print()
+    #     print(cout(listeVillesIP))
 
-        listeV.remove(ville)
+    #     listeV.remove(ville)
 
     return listeVillesIP
 
@@ -178,76 +218,78 @@ if __name__ == "__main__":
     listeVillesCopie = listeVilles.copy()
 
 
-    root = Tk(className='TP recherche opérationnelle')
-    root.geometry("686x754")
+    # root = Tk(className='TP recherche opérationnelle')
+    # root.geometry("686x754")
 
-    photo = PhotoImage(file="carte.png")
-    canv = Canvas(root, width=largeur, height=hauteur)
-    canv.create_image(0, 0, anchor=NW, image=photo)
+    # photo = PhotoImage(file="carte.png")
+    # canv = Canvas(root, width=largeur, height=hauteur)
+    # canv.create_image(0, 0, anchor=NW, image=photo)
 
-    canv.pack(expand=YES, fill="both")
+    # canv.pack(expand=YES, fill="both")
     #============================================================#
 
 
-    listeVillesIP = insertion_proche(listeVillesCopie)
-    # for v in listeVillesIP:
-    #     print(str(v))
+    # listeVillesIP = insertion_proche(listeVillesCopie)
+    # # for v in listeVillesIP:
+    # #     print(str(v))
 
-    listeVillesCopie = listeVilles.copy()
+    # listeVillesCopie = listeVilles.copy()
 
-    for pos in range(0, len(listeVillesCopie)):
-        x, y = fromLatLong2pixels(listeVillesCopie[pos], listeVillesCopie, largeur, hauteur)
-        y = hauteur - y
-        create_circle(x, y, 5, canv)
-        Label(root, text=listeVillesCopie[pos].getNum(), fg="black").place(x=x, y=y)
-        if pos > 0:
-            xPrecedent, yPrecedent = fromLatLong2pixels(listeVillesCopie[pos-1], listeVillesCopie, largeur, hauteur)
-            canv.create_line(xPrecedent, xPrecedent, x, y)
-
-
-
-    root.mainloop()
+    # for pos in range(0, len(listeVillesCopie)):
+    #     x, y = fromLatLong2pixels(listeVillesCopie[pos], listeVillesCopie, largeur, hauteur)
+    #     y = hauteur - y
+    #     create_circle(x, y, 5, canv)
+    #     Label(root, text=listeVillesCopie[pos].getNum(), fg="black").place(x=x, y=y)
+    #     if pos > 0:
+    #         xPrecedent, yPrecedent = fromLatLong2pixels(listeVillesCopie[pos-1], listeVillesCopie, largeur, hauteur)
+    #         canv.create_line(xPrecedent, xPrecedent, x, y)
 
 
-    distanceTotaleOrdre = cout(listeVilles)
-    print(f"Distance totale parcourue ordre croissant: {distanceTotaleOrdre} km")
-    print(f"Chemin utilise dans l'ordre : {afficheTour(listeVilles)}\n\n")
 
-    print(f"Ville la plus proche de {listeVilles[0].getNom()} : {plus_proche(listeVilles, listeVilles[0])[1].getNom()}")
+    # root.mainloop()
+    
+    
+    # listeVillesCopie = listeVilles.copy()
+    # distanceTotaleOrdre = cout(listeVillesCopie)
+    # print(f"Distance totale parcourue ordre croissant: {distanceTotaleOrdre} km")
+    # print(f"Chemin utilise dans l'ordre : {afficheTour(listeVilles)}\n\n")
 
-    listeVillesCopie = listeVilles.copy()
+    # print(f"Ville la plus proche de {listeVilles[0].getNom()} : {plus_proche(listeVilles, listeVilles[0])[1].getNom()}")
 
-
-    listeVillesCopie = tourAleatoire(listeVillesCopie)
-    distanceTotale = cout(listeVillesCopie)
-    print(f"Distance totale parcourue aleatoire: {distanceTotale} km")
-    print(f"Chemin utilise aleatoire : {afficheTour(listeVillesCopie)}\n\n")
-
-    listeVillesCopie = listeVilles.copy()
+    # listeVillesCopie = listeVilles.copy()
 
 
-    tournee_glouton = plus_proche_voisin(listeVillesCopie, listeVilles[0])
-    print(f"Chemin utilise dans glouton : {afficheTour(tournee_glouton)}")
-    print(f"Distance totale parcourue glouton : {cout(tournee_glouton)} km\n\n")
+    # listeVillesCopie = tourAleatoire(listeVillesCopie)
+    # distanceTotale = cout(listeVillesCopie)
+    # print(f"Distance totale parcourue aleatoire: {distanceTotale} km")
+    # print(f"Chemin utilise aleatoire : {afficheTour(listeVillesCopie)}\n\n")
+
+    # listeVillesCopie = listeVilles.copy()
+
+    # print("Ville 1 " + listeVillesCopie[0].getNom())
+    # tournee_glouton = plus_proche_voisin(listeVillesCopie, listeVilles[0])
+    # print(f"Chemin utilise dans glouton : {afficheTour(tournee_glouton)}")
+    # print(f"Distance totale parcourue glouton : {cout(tournee_glouton)} km\n\n")
 
     #On rempli listeVillesCopie vu que celle-ci se vide dans la méthode plus_proche_voisin
-    listeVillesCopie = listeVilles.copy()
+    # listeVillesCopie = listeVilles.copy()
 
-    tournee_glouton_ameliore = plus_proche_voisin_ameliore(listeVillesCopie)
-    print(f"Chemin utilise dans glouton ameliore : {afficheTour(tournee_glouton_ameliore)}")
-    print(f"Distance totale parcourue glouton ameliore: {cout(tournee_glouton_ameliore)} km\n\n")
+    # tournee_glouton_ameliore = plus_proche_voisin_ameliore(listeVillesCopie)
+    # print(f"Chemin utilise dans glouton ameliore : {afficheTour(tournee_glouton_ameliore)}")
+    # print(f"Distance totale parcourue glouton ameliore: {cout(tournee_glouton_ameliore)} km\n\n")
 
     
     listeVillesCopie = listeVilles.copy()
+    
 
 
-    listeVillesIP = insertion_proche(listeVillesCopie)
-    print(f"Chemin utilise dans insertion proche : {afficheTour(listeVillesIP)}")
-    print(f"Distance totale parcourue insertion proche : {cout(listeVillesIP)} km\n\n")
+    listeVillesIP1 = insertion_proche(listeVillesCopie)
+    print(f"Chemin utilise dans insertion proche : {afficheTour(listeVillesIP1)}")
+    print(f"Distance totale parcourue insertion proche : {cout(listeVillesIP1)} km\n\n")
 
-    listeVillesCopie = listeVilles.copy()
+    # listeVillesCopie = listeVilles.copy()
 
 
-    listeVillesIL = insertion_loin(listeVillesCopie)
-    print(f"Chemin utilise dans insertion loin : {afficheTour(listeVillesIL)}")
-    print(f"Distance totale parcourue insertion loin : {cout(listeVillesIL)} km\n\n")
+    # listeVillesIL = insertion_loin(listeVillesCopie)
+    # print(f"Chemin utilise dans insertion loin : {afficheTour(listeVillesIL)}")
+    # print(f"Distance totale parcourue insertion loin : {cout(listeVillesIL)} km\n\n")
