50ba205c5d48bf8e6620deea4fa1595d6101c7f0
Adrien <adrien.bony38@gmail.com>
2022-03-16 11:58:11 +0100

    2 opt

--- main.py
+++ main.py
@@ -10,20 +10,17 @@ def affiche_tournee(listeVAAFF):
     canv.create_image(0, 0, anchor=NW, image=photo)
 
     canv.pack(expand=YES, fill="both")
-    for i in range(0, len(listeVAAFF)-1):
+    for i in range(0, len(listeVAAFF)):
         x, y = fromLatLong2pixels(listeVAAFF[i], listeVAAFF, largeur, hauteur)
-        xSuiv, ySuiv = fromLatLong2pixels(listeVAAFF[i+1], listeVAAFF, largeur, hauteur)
-        y = hauteur - y
+        xSuiv, ySuiv = fromLatLong2pixels(listeVAAFF[(i+1)%len(listeVAAFF)], listeVAAFF, largeur, hauteur)
+        y = hauteur-y
         ySuiv = hauteur - ySuiv
-
-        canv.create_line(x, y, xSuiv, ySuiv)
+            
+        canv.create_line(x, y, xSuiv, ySuiv)  
         canv.create_text(x, y, text=str(listeVAAFF[i].getNum()))
         canv.pack()
 
-        if (i == len(listeVAAFF)-1):
-            xSuiv, ySuiv = fromLatLong2pixels(listeVAAFF[0], listeVAAFF, largeur, hauteur)
-            ySuiv = hauteur - ySuiv
-            canv.create_line(x, y, xSuiv, ySuiv)
+        
 
 
 
@@ -97,6 +94,7 @@ def insertion_proche(listeV):
 
 
 def insertion_loin(listeV):
+
     # on cherche les deux villes les plus éloignées l'une de l'autre
     listeVillesIL = get_villes_plus_eloignees(listeV)
     
@@ -104,34 +102,43 @@ def insertion_loin(listeV):
     if(listeVillesIL[0] in listeV and listeVillesIL[1] in listeV):
         listeV.remove(listeVillesIL[0])
         listeV.remove(listeVillesIL[1])
-        
-    for v in listeVillesIL:
-        print(v.getNum(), end=' ')
-        
-    for i in range(0, 2):
-        distance_max = -1
+    
+    # chaque itération permet de trouver la ville qui étend le moins la tournée
+    for i in range(0, len(listeV)):
+        distance_max = 0
         index_min = -1
-        distance_precedente = 9999999999999
         ville = None
-
+        
+        temp_index = -1
+        temp_ville = None
+        # on va parcourir la liste de villes pour trouver la ville qui étend le moins la tournée
         for v in listeV:
 
+            distance_min = 9999999999
+            # on va chercher l'index auquel la ville ajoutera le moins de distance
             for i in range(0, len(listeVillesIL)):
-
+                # on vérifie si la ville peut prendre la dernière place de la tournée
                 if(i == len(listeVillesIL)-1):
                     distance_ajoutee = calculDistance(listeVillesIL[i], v) + calculDistance(v, listeVillesIL[0]) - calculDistance(listeVillesIL[i], listeVillesIL[0])
+                # sinon, pour tous les autres éléments de la liste, on calcule la distance ajoutée
                 else:
                     distance_ajoutee = calculDistance(listeVillesIL[i], v) + calculDistance(v, listeVillesIL[i+1]) - calculDistance(listeVillesIL[i], listeVillesIL[i+1])
                 
-                if(distance_ajoutee > distance_max and distance_ajoutee < distance_precedente):
-                    if(i == len(listeVillesIL)-1 and i > 0):
-                        distance_ajoutee = calculDistance(listeVillesIL[i], ville) + calculDistance(ville, listeVillesIL[0]) - calculDistance(listeVillesIL[i], listeVillesIL[0])
-                    elif(i > 0):
-                        distance_precedente = calculDistance(listeVillesIL[i], ville) + calculDistance(ville, listeVillesIL[i+1]) - calculDistance(listeVillesIL[i], listeVillesIL[i+1])
-                    distance_max = distance_ajoutee
-                    index_min = i
-                    ville = v
+                # si la nouvelle distance est mieux, alors on la prends.
+                if(distance_ajoutee < distance_min):
+                    distance_min = distance_ajoutee
+                    temp_index = i
+                    temp_ville = v
+            
+            if(distance_min > distance_max and distance_min != 9999999999):
+                distance_max = distance_min
+                index_min = temp_index
+                ville = temp_ville
+                
+        
+        # une fois que la ville et l'index minimum sont trouvés, on insère dans la nouvelle tournée
         listeVillesIL.insert(index_min+1, ville)
+        # on enlève la ville de a liste de villes pour éviter qu'elle apparaisse plusieurs fois
         listeV.remove(ville)
 
     return listeVillesIL
@@ -143,8 +150,9 @@ def recherche_locale(listeVL):
     fini = False
     while not fini:
         fini = True
-        t_voisin = []
-        if(cout(t_voisin) < cout(t_courante)):
+        cout_t_courante = cout(t_courante)
+        t_voisin = exploration_successeurs_premier_dabord(t_courante)
+        if(cout(t_voisin) < cout_t_courante):
             t_courante = t_voisin
             fini = False
     return t_courante
@@ -153,9 +161,41 @@ def recherche_locale(listeVL):
 
 def exploration_successeurs_premier_dabord(t_courante):
     for i in range(0, len(t_courante)):
-        if(calculDistance(t_courante[i-1], t_courante[i]) + calculDistance(t_courante[i+1], t_courante[i+2]) > calculDistance(t_courante[i-1], t_courante[i+1]) + calculDistance(t_courante[i], t_courante[i+2])):
-            print("ok")
-            
+        if(calculDistance(t_courante[(i-1)%len(t_courante)], t_courante[i]) + calculDistance(t_courante[(i+1)%len(t_courante)], t_courante[(i+2)%len(t_courante)]) > calculDistance(t_courante[(i-1)%len(t_courante)], t_courante[(i+1)%len(t_courante)]) + calculDistance(t_courante[i], t_courante[(i+2)%len(t_courante)])):
+            #swap les deux positions
+            t_courante[i], t_courante[i+1] = t_courante[i+1], t_courante[i]
+    return t_courante
+
+
+def echange_sommets_quelconques(t_courante):
+    fini = False
+    while not fini:
+        fini = True
+        for i in range(0, len(t_courante)):
+            for j in range(i+1, len(t_courante)):
+                cout_temp = cout(t_courante)
+                t_courante[i], t_courante[j] = t_courante[j], t_courante[i]
+                if(cout(t_courante) > cout_temp):
+                    t_courante[i], t_courante[j] = t_courante[j], t_courante[i]
+                else:
+                    fini = False
+    return t_courante
+
+
+def echange_2_opt(t_courante):
+    fini = False
+    while not fini:
+        fini = True
+        for i in range(0, len(t_courante)):
+            for j in range(i+2, len(t_courante)):
+                cout_temp = cout(t_courante)
+                t_courante[i:j] = t_courante[i:j][::-1]
+                if(cout(t_courante) > cout_temp):
+                    t_courante[i:j] = t_courante[i:j][::-1]
+                else:
+                    fini = False
+    return t_courante
+
 
 if __name__ == "__main__":
 
@@ -199,10 +239,10 @@ if __name__ == "__main__":
     # listeVillesCopie = listeVilles.copy()
 
 
-    # listeVillesCopie = tourAleatoire(listeVillesCopie)
-    # distanceTotale = cout(listeVillesCopie)
-    # print(f"Distance totale parcourue aleatoire: {distanceTotale} km")
-    # print(f"Chemin utilise aleatoire : {afficheTour(listeVillesCopie)}\n\n")
+    # tourneeAlea = tourAleatoire(listeVillesCopie)
+    # distanceTotale = cout(tourneeAlea)
+    # print(f"Distance totale parcourue aleatoire: {cout(tourneeAlea)} km")
+    # print(f"Chemin utilise aleatoire : {afficheTour(tourneeAlea)}\n\n")
 
 
 
@@ -210,12 +250,12 @@ if __name__ == "__main__":
     # METHODES GLOUTONNES
 
 
-    # listeVillesCopie = listeVilles.copy()
+    listeVillesCopie = listeVilles.copy()
 
-    # print("Ville 1 " + listeVillesCopie[0].getNom())
-    # tournee_glouton = plus_proche_voisin(listeVillesCopie, listeVilles[0])
-    # print(f"Chemin utilise dans glouton : {afficheTour(tournee_glouton)}")
-    # print(f"Distance totale parcourue glouton : {cout(tournee_glouton)} km\n\n")
+    print("Ville 1 " + listeVillesCopie[0].getNom())
+    tournee_glouton = plus_proche_voisin(listeVillesCopie, listeVilles[0])
+    print(f"Chemin utilise dans glouton : {afficheTour(tournee_glouton)}")
+    print(f"Distance totale parcourue glouton : {cout(tournee_glouton)} km\n\n")
 
 
     # listeVillesCopie = listeVilles.copy()
@@ -224,7 +264,7 @@ if __name__ == "__main__":
     # print(f"Chemin utilise dans glouton ameliore : {afficheTour(tournee_glouton_ameliore)}")
     # print(f"Distance totale parcourue glouton ameliore: {cout(tournee_glouton_ameliore)} km\n\n")
 
-
+    
     # listeVillesCopie = listeVilles.copy()
     
     # listeVillesIP1 = insertion_proche(listeVillesCopie)
@@ -237,9 +277,22 @@ if __name__ == "__main__":
     # listeVillesIL = insertion_loin(listeVillesCopie)
     # print(f"Chemin utilise dans insertion loin : {afficheTour(listeVillesIL)}")
     # print(f"Distance totale parcourue insertion loin : {cout(listeVillesIL)} km\n\n")
+    
 
-    listeVillesIP = insertion_proche(listeVillesCopie)
+    # listeRechercheLocale = recherche_locale(tournee_glouton)
+    # print(f"Chemin utilise dans recherche locale : {afficheTour(listeRechercheLocale)}")
+    # print(f"Distance totale parcourue recherche locale : {cout(listeRechercheLocale)} km\n\n")
+    
+    # liste_sommets_qq = echange_sommets_quelconques(tournee_glouton)
+    # print(f"Chemin utilise dans recherche sommets quelconques : {afficheTour(liste_sommets_qq)}")
+    # print(f"Distance totale parcourue recherche sommets quelconques : {cout(liste_sommets_qq)} km\n\n")
+    
+    liste_echange_2_opt = echange_2_opt(tournee_glouton)
+    print(f"Chemin utilise dans recherche sommets quelconques : {afficheTour(liste_echange_2_opt)}")
+    print(f"Distance totale parcourue recherche sommets quelconques : {cout(liste_echange_2_opt)} km\n\n")
 
-    affiche_tournee(listeVillesIP, listeVillesIP)
+    affiche_tournee(liste_echange_2_opt)
+
+    
 
     root.mainloop()
